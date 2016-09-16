acfe0095d068ed7ee899f6fbd77982dbc5baa71e
Joseph Roussy <jroussy@uwo.ca>
2016-09-15 22:35:16 -0400

    Added a 2d array to the window object to track the state of the game

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 document.addEventListener('DOMContentLoaded', function (e) {
 
+    var GAME_ROOT_LENGTH = 70
+    var CELL_LENGTH = 10
+
+    initializeGameState(GAME_ROOT_LENGTH, GAME_ROOT_LENGTH)
+
     var gameRoot = document.getElementById('gameRoot')
-    populateGridElements(gameRoot)
+    populateGridElements(gameRoot, GAME_ROOT_LENGTH, CELL_LENGTH)
+
+    // TODO: Set interval for each iteration of the game
 
 })
+
+function initializeGameState(numberOfRows, numberOfCellsPerRow) {
+    var gameState = new Array(numberOfRows)
+
+    for (var i = 0; i<gameState.length; i++) {
+        gameState[i] = new Array(numberOfCellsPerRow)
+    }
+
+    window.gameState = gameState
+}
 
 function randomBinary() {
     return Math.floor(Math.random() + 0.5)
@@ -15,25 +32,26 @@ function getDiv(className) {
     return el
 }
 
-function populateGridElements(gameRoot) {
-    var GAME_ROOT_LENGTH = 70
-    var CELL_LENGTH = 10
+function populateGridElements(gameRoot, gameRootLength, cellLength) {
 
-    for (var i=0; i<GAME_ROOT_LENGTH; i++) {
+    for (var i=0; i<gameRootLength; i++) {
         var newRow = getDiv('row')
 
-        for (var j=0; j<GAME_ROOT_LENGTH; j++) {
+        for (var j=0; j<gameRootLength; j++) {
             var newCell = getDiv('cell')
 
             if (randomBinary()) {
                 newCell.classList.add('on')
             }
 
-            newCell.style.left = j*CELL_LENGTH + 'px'
+            newCell.style.left = j*cellLength + 'px'
             newRow.appendChild(newCell)
+            window.gameState[i][j] = newCell
         }
 
-        newRow.style.top = i*CELL_LENGTH + 'px'
+        newRow.style.top = i*cellLength + 'px'
         gameRoot.appendChild(newRow)
     }
 }
+
+// TODO: Create function to run on each iteration of the game
