4d74bccdf8eba0379304eda89d4c33d3061dab1c
Joseph Roussy <joeroussy@gmail.com>
2016-09-17 16:01:44 -0400

    Removed frame rate measuring

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ document.addEventListener('DOMContentLoaded', function (e) {
     var GAME_ROOT_LENGTH = 70
     var CELL_LENGTH = 10
 
-    window.timeStamp = +new Date()
-
     initializeGameState(GAME_ROOT_LENGTH, GAME_ROOT_LENGTH)
 
     var gameRoot = document.getElementById('gameRoot')
@@ -200,10 +198,6 @@ function setCellState(cell) {
 }
 
 function onGameTick(gameRootLength) {
-    var currentTime = +new Date()
-    console.log(currentTime - timeStamp)
-    timeStamp = currentTime
-
     for (var i=0; i<gameRootLength; i++) {
         for (var j=0; j<gameRootLength; j++) {
             setCellState(gameState[i][j])
